6a0adbb65e8e340fb3618801f15aadf9f8ec977f
Thomas R. Benson <benson31@llnl.gov>
2017-10-11 12:47:23 -0700

    Massive delete of files extraneous to hydrogen

--- include/El.hpp
+++ include/El.hpp
@@ -2,8 +2,8 @@
    Copyright (c) 2009-2016, Jack Poulson
    All rights reserved.
 
-   This file is part of Elemental and is under the BSD 2-Clause License, 
-   which can be found in the LICENSE file in the root directory, or at 
+   This file is part of Elemental and is under the BSD 2-Clause License,
+   which can be found in the LICENSE file in the root directory, or at
    http://opensource.org/licenses/BSD-2-Clause
 */
 #ifndef EL_HPP
@@ -15,12 +15,9 @@
 #include <El/blas_like.hpp>
 
 #include <El/lapack_like.hpp>
-#include <El/optimization.hpp>
 #include <El/control.hpp>
 
 #include <El/matrices.hpp>
-
-#include <El/number_theory.hpp>
 
 #include <El/io.hpp>
 
